43a03c62483baad34f7cb756009e36c93c1f4e58
gallantdevs <gallantdevs@gmail.com>
2026-01-26 19:55:46 +0000

    boom-testing3

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 // import img1 from '../../public/1.png'
 // import img2 from '../../public/2.png'
 // import img3 from '../../public/3.jpeg' 
-import img1 from '../../public/A.jpeg'
+import img1 from '../../public/AA34.jpg'
 import img2 from '../../public/B.jpeg'
 import img3 from '../../public/C.jpeg' 
 
